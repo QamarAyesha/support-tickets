157e7fcae7c5cbc1a94f334cfcad59f81815f494
QamarAyesha <138522264+QamarAyesha@users.noreply.github.com>
2025-02-23 00:11:27 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ if "df" not in st.session_state:
     # Generate the dataframe with 100 rows/tickets.
     data = {
         "ID": [f"TICKET-{i}" for i in range(1100, 1000, -1)],
-        "Issue": np.random.choice(task_descriptions, size=100),
+        "Issue": np.random.choice(issue_descriptions, size=100),
         "Status": np.random.choice(["Open", "In Progress", "Closed"], size=100),
         "Priority": np.random.choice(["High", "Medium", "Low"], size=100),
         "Date Submitted": [
